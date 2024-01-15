89b3d62865f083ebbcd7332b87ca322f2ddd196c
Max Melching <max.melching@aei.mpg.de>
2024-01-15 20:15:45 +0100

    Made testing more serious. Added separate .py files that can be executed using pytest.

---
+++ tests/test_inner_product.py
@@ -0,0 +1,157 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+from gw_signal_tools.inner_product import inner_product, norm, overlap
+from gw_signal_tools.PSDs import psd_gw150914
+
+import astropy.units as u
+import lalsimulation.gwsignal.core.waveform as wfm
+
+
+#%% ---------- Consistency tests with inner_product function ----------
+
+deltaT = 1./2048.*u.s
+f_min = 20.*u.Hz  # Cutoff frequency
+f_ref = 20.*u.Hz  # Frequency where we specify spins
+distance = 440.*u.Mpc
+inclination = 2.7*u.rad  # Value taken from posteriors.ipynb, where posterior of inclination is plotted
+phiRef = 0.*u.rad
+eccentricity = 0.*u.dimensionless_unscaled
+longAscNodes = 0.*u.rad
+meanPerAno = 0.*u.rad
+
+
+wf_params = {
+    'mass1' : 36*u.solMass,
+    'mass2' : 29*u.solMass,
+    'deltaT' : deltaT,  # Needed even for FDWaveform, otherwise f_max very small
+    # 'deltaF' : deltaT / u.s * u.Hz,  # Does work
+    'f22_start' : f_min,
+    'f22_ref': f_ref,
+    'phi_ref' : phiRef,
+    'distance' : distance,
+    'inclination' : inclination,
+    'eccentricity' : eccentricity,
+    'longAscNodes' : longAscNodes,
+    'meanPerAno' : meanPerAno,
+    'condition' : 0
+}
+
+approximant = 'IMRPhenomXPHM'
+
+# Call the generator
+gen = wfm.LALCompactBinaryCoalescenceGenerator(approximant)
+
+
+# Generate time domain waveform
+hp_t, _ = wfm.GenerateTDWaveform(wf_params, gen)
+
+# Two waveforms will be generated in frequency domain, first with finer
+# sampling and then with coarser one
+hp_f_fine, _ = wfm.GenerateFDWaveform(wf_params, gen)
+
+hp_f_coarse, _ = wfm.GenerateFDWaveform(wf_params | {'deltaF': 1.0 / (hp_t.size * hp_t.dx)}, gen)
+
+
+def test_fd_td_match_consistency():
+    match_td = norm(hp_t, psd_gw150914, df=2**-2, f_range=[f_min, None])
+    match_fd = norm(hp_f_coarse, psd_gw150914, df=2**-2, f_range=[f_min, None])
+
+    assert np.isclose(match_td, match_fd, atol=0.0, rtol=0.005)
+
+    match_td = norm(hp_t, psd_gw150914, df=2**-4, f_range=[f_min, None])
+    match_fd = norm(hp_f_fine, psd_gw150914, df=2**-4, f_range=[f_min, None])
+
+    assert np.isclose(match_td, match_fd, atol=0.0, rtol=0.005)
+
+
+def test_norm():
+    norm_td = overlap(hp_t, hp_t, psd_gw150914, df=2**-4, f_range=[f_min, None])
+    norm_fd_coarse = overlap(hp_f_coarse, hp_f_coarse, psd_gw150914, df=2**-2, f_range=[f_min, None])
+    norm_fd_fine = overlap(hp_f_fine, hp_f_fine, psd_gw150914, df=2**-4, f_range=[f_min, None])
+
+    assert np.all(np.isclose([norm_td, norm_fd_coarse, norm_fd_fine], [1.0, 1.0, 1.0], atol=0.0, rtol=0.005))
+
+
+#%% ---------- Confirming results with PyCBC match function ----------
+
+from pycbc.waveform import get_fd_waveform
+from pycbc.filter import match
+from pycbc.psd import aLIGOZeroDetHighPower
+
+from gwpy.frequencyseries.frequencyseries import FrequencySeries
+
+f_low, f_high = 20, 350  # f_min and some essentially arbitrary cutoff
+sample_rate = 4096
+
+# Enter some arbitrary parameters here
+wfs_to_compare = {
+    'signal1': {
+        'mass1': 10,
+        'mass2': 10,
+        'spin1': 0.6,
+        'spin2': 0.0
+    },
+    'signal2':{
+        'mass1': 96,
+        'mass2': 20,
+        'spin1': 0.0,
+        'spin2': 0.1
+    }
+}
+
+
+hp_1_pycbc, _ = get_fd_waveform(
+    approximant=approximant,
+    **wfs_to_compare['signal1'],
+    f_lower=f_low,
+    f_upper=f_high,
+    delta_f=1.0/sample_rate
+)
+
+hp_2_pycbc, _ = get_fd_waveform(
+    approximant=approximant,
+    **wfs_to_compare['signal2'],
+    f_lower=f_low,
+    f_upper=f_high,
+    delta_f=1.0/sample_rate
+)
+
+tlen = max(len(hp_1_pycbc), len(hp_2_pycbc))
+hp_1_pycbc.resize(tlen)
+hp_2_pycbc.resize(tlen)
+
+delta_f = 1.0 / hp_2_pycbc.duration
+flen = tlen//2 + 1
+psd_pycbc = aLIGOZeroDetHighPower(flen, delta_f, f_low)
+
+
+hp_1_pycbc_converted = FrequencySeries.from_pycbc(hp_1_pycbc)
+hp_2_pycbc_converted = FrequencySeries.from_pycbc(hp_2_pycbc)
+psd_pycbc_converted = FrequencySeries.from_pycbc(psd_pycbc)
+
+
+def test_match_pycbc():
+    overlap_pycbc, _ = match(hp_1_pycbc, hp_2_pycbc, v1_norm=1.0, v2_norm=1.0, psd=psd_pycbc, low_frequency_cutoff=f_low, high_frequency_cutoff=f_high)
+
+    _, overlap_gw_signal_tools, _ = inner_product(hp_1_pycbc_converted, hp_2_pycbc_converted, psd_pycbc_converted, f_range=[f_low, f_high], optimize_time_and_phase=True)
+
+    assert np.isclose(overlap_pycbc, overlap_gw_signal_tools, atol=0.0, rtol=0.01)
+
+
+def test_overlap_pycbc():
+    overlap_normalized_pycbc, _ = match(hp_1_pycbc, hp_2_pycbc, psd=psd_pycbc, low_frequency_cutoff=f_low, high_frequency_cutoff=f_high)
+
+    _, overlap_normalized_gw_signal_tools, _ = overlap(hp_1_pycbc_converted, hp_2_pycbc_converted, psd_pycbc_converted, f_range=[f_low, f_high], optimize_time_and_phase=True)
+
+    # assert np.abs((overlap_normalized_pycbc - overlap_normalized) / overlap_normalized_pycbc) < 0.01
+    assert np.isclose(overlap_normalized_pycbc, overlap_normalized_gw_signal_tools, atol=0.0,rtol=0.01)  # TODO: change rtol to 0.005?
+
+
+def test_norm_optimized():
+    _, norm1_gw_signal_tools, _ = overlap(hp_1_pycbc_converted, hp_1_pycbc_converted, psd_pycbc_converted, f_range=[f_low, f_high], optimize_time_and_phase=True)
+    _, norm2_gw_signal_tools, _ = overlap(hp_2_pycbc_converted, hp_2_pycbc_converted, psd_pycbc_converted, f_range=[f_low, f_high], optimize_time_and_phase=True)
+
+    assert np.isclose(norm1_gw_signal_tools, 1.0, atol=0.0, rtol=0.01)
+    assert np.isclose(norm2_gw_signal_tools, 1.0, atol=0.0, rtol=0.01)
---
+++ tests/test_waveform_utils.py
@@ -0,0 +1,161 @@
+from gw_signal_tools.inner_product import td_to_fd_waveform, fd_to_td_waveform, pad_to_get_target_df, restrict_f_range
+
+import astropy.units as u
+import lalsimulation.gwsignal.core.waveform as wfm
+import numpy as np
+
+
+# We will perform tests with a GW150914-like signal
+deltaT = 1./2048.*u.s
+f_min = 20.*u.Hz
+f_ref = 20.*u.Hz
+distance = 440.*u.Mpc
+inclination = 2.7*u.rad
+phiRef = 0.*u.rad
+eccentricity = 0.*u.dimensionless_unscaled
+longAscNodes = 0.*u.rad
+meanPerAno = 0.*u.rad
+
+
+wf_params = {
+    'mass1' : 36*u.solMass,
+    'mass2' : 29*u.solMass,
+    'deltaT' : deltaT,
+    'f22_start' : f_min,
+    'f22_ref': f_ref,
+    'phi_ref' : phiRef,
+    'distance' : distance,
+    'inclination' : inclination,
+    'eccentricity' : eccentricity,
+    'longAscNodes' : longAscNodes,
+    'meanPerAno' : meanPerAno,
+    'condition' : 0
+}
+
+approximant = 'IMRPhenomXPHM'
+
+gen = wfm.LALCompactBinaryCoalescenceGenerator(approximant)
+
+hp_t, _ = wfm.GenerateTDWaveform(wf_params, gen)
+
+
+hp_f_fine, _ = wfm.GenerateFDWaveform(wf_params, gen)
+
+hp_f_coarse, _ = wfm.GenerateFDWaveform(wf_params | {'deltaF': 1.0 / (hp_t.size * hp_t.dx)}, gen)
+
+
+#%% ---------- Testing transformation into one domain and back ----------
+def test_ifft_fft_consistency():
+    f_min_comp, f_max_comp = 20.0 * u.Hz, 512.0 * u.Hz  # Restrict to interesting region, elsewhere only values close to zero and thus numerical errors might occur
+
+    hp_f_coarse_ifft_fft = td_to_fd_waveform(fd_to_td_waveform(hp_f_coarse))
+
+    hp_f_coarse_cropped = hp_f_coarse.crop(start=f_min_comp, end=f_max_comp)
+    hp_f_coarse_ifft_fft_cropped = hp_f_coarse_ifft_fft.crop(start=f_min_comp, end=f_max_comp)
+
+    assert np.all(np.isclose(np.real(hp_f_coarse_cropped), np.real(hp_f_coarse_ifft_fft_cropped), atol=0.0, rtol=0.001))
+
+    hp_f_fine_ifft_fft = td_to_fd_waveform(fd_to_td_waveform(hp_f_fine))
+
+    hp_f_fine_cropped = hp_f_fine.crop(start=f_min_comp, end=f_max_comp)
+    hp_f_fine_ifft_fft_cropped = hp_f_fine_ifft_fft.crop(start=f_min_comp, end=f_max_comp)
+
+    assert np.all(np.isclose(np.real(hp_f_fine_cropped), np.real(hp_f_fine_ifft_fft_cropped), atol=0.0, rtol=0.001))
+
+
+def test_fft_ifft_consistency():
+    hp_t_fft_ifft = fd_to_td_waveform(td_to_fd_waveform(hp_t))
+
+    t_min_comp, t_max_comp = max(hp_t.t0, hp_t_fft_ifft.t0), min(hp_t.times[-1], hp_t_fft_ifft.times[-1])  # hp_t_fft_ifft_fine is padded to be much longer
+    # t_min_comp, t_max_comp = -0.5, 0.01  # hp_t_fft_ifft_fine is padded to be much longer
+
+    hp_t_cropped = hp_t.crop(start=t_min_comp, end=t_max_comp)[2:]
+    hp_t_fft_ifft_cropped = hp_t_fft_ifft.crop(start=t_min_comp, end=t_max_comp)
+
+    assert np.all(np.isclose(hp_t_cropped, hp_t_fft_ifft_cropped, atol=1.2e-23, rtol=0.0))
+    # Have to apply different kind of threshold here because coarse sampling is indeed very coarse
+
+    hp_t_fft_ifft_fine = fd_to_td_waveform(td_to_fd_waveform(pad_to_get_target_df(hp_t, df=0.0625 * u.Hz)))
+
+    t_min_comp, t_max_comp = max(hp_t.t0, hp_t_fft_ifft_fine.t0), min(hp_t.times[-1], hp_t_fft_ifft_fine.times[-1])  # hp_t_fft_ifft_fine is padded to be much longer
+    
+    hp_t_cropped = hp_t.crop(start=t_min_comp, end=t_max_comp)[1:]
+    hp_t_fft_ifft_fine_cropped = hp_t_fft_ifft_fine.crop(start=t_min_comp, end=t_max_comp)[1:]
+    # NOTE: for some reason, first sample is not equal. Thus excluded here
+
+    assert np.all(np.isclose(hp_t_cropped, hp_t_fft_ifft_fine_cropped, atol=0.0, rtol=0.001))
+    
+
+#%% ---------- Testing transformations with generated signals from different domain ----------
+def test_fd_td_consistency():
+    # NOTE: we have to apply different thresholds for certain frequency regions here.
+    # For f_min_comp close to f_min from the parameter dictionary above, the threshold
+    # has to be chosen a bit higher than the usual 1%. Here, it comes into play
+    # that tapering is applied to TDWaveform that we do FFT of, while this is not
+    # done for FDWaveform. This causes certain differences in the Fourier components
+
+    f_min_comp, f_max_comp = 20.0 * u.Hz, 512.0 * u.Hz  # Restrict to interesting region, elsewhere only values close to zero and thus numerical errors might occur
+    
+    hp_t_f_coarse = td_to_fd_waveform(hp_t)
+
+    hp_f_coarse_cropped = hp_f_coarse.crop(start=f_min_comp, end=f_max_comp)
+    hp_t_f_coarse_cropped = hp_t_f_coarse.crop(start=f_min_comp, end=f_max_comp)
+
+    assert np.all(np.isclose(hp_f_coarse_cropped, hp_t_f_coarse_cropped, atol=0.0, rtol=0.05))
+
+    # For a finer resolution, we have to pad signal
+    hp_t_padded = pad_to_get_target_df(hp_t, df=hp_f_fine.df)
+    hp_t_f_fine = td_to_fd_waveform(hp_t_padded)
+
+    hp_f_fine_cropped = hp_f_fine.crop(start=f_min_comp, end=f_max_comp)
+    hp_t_f_fine_cropped = hp_t_f_fine.crop(start=f_min_comp, end=f_max_comp)
+
+    assert np.all(np.isclose(hp_f_fine_cropped, hp_t_f_fine_cropped, atol=0.0, rtol=0.05))
+
+
+
+    f_min_comp, f_max_comp = 25.0 * u.Hz, 512.0 * u.Hz  # Restrict to interesting region, elsewhere only values close to zero and thus numerical errors might occur
+    
+    hp_t_f_coarse = td_to_fd_waveform(hp_t)
+
+    hp_f_coarse_cropped = hp_f_coarse.crop(start=f_min_comp, end=f_max_comp)
+    hp_t_f_coarse_cropped = hp_t_f_coarse.crop(start=f_min_comp, end=f_max_comp)
+
+    assert np.all(np.isclose(hp_f_coarse_cropped, hp_t_f_coarse_cropped, atol=0.0, rtol=0.01))
+
+    # For a finer resolution, we have to pad signal
+    hp_t_padded = pad_to_get_target_df(hp_t, df=hp_f_fine.df)
+    hp_t_f_fine = td_to_fd_waveform(hp_t_padded)
+
+    hp_f_fine_cropped = hp_f_fine.crop(start=f_min_comp, end=f_max_comp)
+    hp_t_f_fine_cropped = hp_t_f_fine.crop(start=f_min_comp, end=f_max_comp)
+
+    assert np.all(np.isclose(hp_f_fine_cropped, hp_t_f_fine_cropped, atol=0.0, rtol=0.01))
+
+
+#%% ---------- Testing helper functions ----------
+    
+def test_restrict_f_range():
+    f_crop_low, f_crop_high = 20.0 * u.Hz, 30.0 * u.Hz
+
+    hp_t_f_coarse = td_to_fd_waveform(hp_t)
+    hp_t_f_coarse_restricted = restrict_f_range(hp_t_f_coarse, f_range=[f_crop_low, f_crop_high])
+    
+    f_min_comp, f_max_comp = f_crop_low + hp_t_f_coarse_restricted.df * 3.0 / 2.0, f_crop_high
+    # Series.crop() uses floor also for lower limit, but restrict_f_range uses ceil. Have to correct for that
+    hp_t_f_coarse_cropped = hp_t_f_coarse.crop(start=f_min_comp, end=f_max_comp)
+    hp_t_f_coarse_rescricted_cropped = hp_t_f_coarse_restricted.crop(start=f_min_comp, end=f_max_comp)
+
+    # f_min_comp, f_max_comp = 20.0 * u.Hz, 30.0 * u.Hz
+    # hp_t_f_coarse_cropped = hp_t_f_coarse[(hp_t_f_coarse.frequencies > f_min_comp) & (hp_t_f_coarse.frequencies < f_max_comp)]
+    # hp_t_f_coarse_rescricted_cropped = hp_t_f_coarse_restricted[(hp_t_f_coarse_restricted.frequencies > f_min_comp) & (hp_t_f_coarse_restricted.frequencies < f_max_comp)]
+
+    assert np.all(np.isclose(hp_t_f_coarse_cropped, hp_t_f_coarse_rescricted_cropped, atol=0.0, rtol=0.001))
+
+    # Also check that everything has been set to zero outside of f_range
+    f_min_comp, f_max_comp = f_crop_low, f_crop_high + hp_t_f_coarse_restricted.df * 3.0 / 2.0
+    hp_t_f_coarse_rescricted_cropped_2 = hp_t_f_coarse_restricted.crop(end=f_min_comp)
+    hp_t_f_coarse_rescricted_cropped_3 = hp_t_f_coarse_restricted.crop(start=f_max_comp)
+
+    assert np.all(np.isclose(0.0, hp_t_f_coarse_rescricted_cropped_2, atol=0.0, rtol=0.001))
+    assert np.all(np.isclose(0.0, hp_t_f_coarse_rescricted_cropped_3, atol=0.0, rtol=0.001))

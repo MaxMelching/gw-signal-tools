399ff6ec31fc9212f90813d954c1d2b4533004d8
Max Melching <max.melching@aei.mpg.de>
2024-02-11 12:53:09 +0100

    Simplified version handling.

--- setup.py
+++ setup.py
@@ -2,31 +2,47 @@ import setuptools
 import os
 
 
-# We paste README into long_description, need read function for this
+# Need read function to paste README into long_description
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
+# Great example: https://git.ligo.org/lscsoft/lalsuite/-/blob/master/wheel/setup.py.in
 setuptools.setup(
+    # ----- Metadata -----
     name='gw_signal_tools',
+    use_scm_version=True,
     author='Frank Ohme, Max Melching',
     author_email='max.melching@aei.mpg.de',
+    maintainer='Max Melching',
+    maintainer_email='max.melching@aei.mpg.de',
     description='Tools for GW Data Analysis',
     long_description=read('README.md'),
     long_description_content_type='text/markdown',
     url='https://gitlab.aei.uni-hannover.de/fohme/gw-signal-tools',
+    project_urls={
+        'Source Code': 'https://gitlab.aei.uni-hannover.de/fohme/gw-signal-tools',
+        # "Documentation": '',
+        "Bug Tracker": 'https://gitlab.aei.uni-hannover.de/fohme/gw-signal-tools/issues'
+    },
     packages=setuptools.find_packages(),
     package_data={'gw_signal_tools': ['PSDs/*.txt']},
     include_package_data=True,
-    zip_safe=False,
+    # ----- Dependencies for installation -----
+    setup_requires=[
+        'setuptools>=64',
+        'setuptools_scm>=8',
+        'wheel'
+    ],
+    # ----- Dependencies for package -----
     python_requires='==3.11.5',  # Installation is verified to work with that
     install_requires=[
         'lalsuite[lalinference]',
         'numpy',
         'scipy',
-        # 'setuptools_scm',  # Is already covered under build-system in toml, thus not needed here
-        'matplotlib'  # TODO: think about moving this to dev
+        'matplotlib'
     ],
+    # ----- Optional dependencies -----
     extras_require={
         'dev': [
             'pycbc',
@@ -38,11 +54,3 @@ setuptools.setup(
         # 'pyseobnr': 'pyseobnr',  # Apparently causes error
     }
 )
-
-# This is strange: if I try to move stuff entirely to pyprojet.toml, an error occurs.
-# However, if I comment build-system stuff in toml, versioning does not work...
-# -> ahhh, figured it out: problem occurs with empty, but existing setup in
-# combination with toml. In that case, two setup files exist (formally), which
-# leads to a conflict. Renaming this file here and using just toml works fine
-# -> more detailed: [project] section of toml is problem; this is also where
-# dynamic part lies, so not sure if this has effect after commenting...

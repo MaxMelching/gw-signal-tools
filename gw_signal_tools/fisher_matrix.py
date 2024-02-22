aa6dab84b4117a9af56cc4c31fd912379fc9cec0
Max Melching <max.melching@aei.mpg.de>
2024-02-22 16:14:17 +0100

    First working version of FisherMatrix. Added FisherMatrix.get_wf_generator function for easy waveform generation. Started implementing some tests as well.

--- gw_signal_tools/fisher_matrix.py
+++ gw_signal_tools/fisher_matrix.py
@@ -1,8 +1,13 @@
+# ----- Standard Lib Imports -----
 import logging
 import warnings
-from typing import Optional, Any, Literal
+from typing import Optional, Any, Literal, Self, Callable
+# from __future__ import annotations  # Hack for type hinting inside a class
+                                    # -> https://stackoverflow.com/a/42845998
 
+# ----- Third Party Imports -----
 import numpy as np
+from numpy.typing import ArrayLike
 import matplotlib.pyplot as plt
 
 from gwpy.types import Series
@@ -10,73 +15,92 @@ from gwpy.frequencyseries import FrequencySeries
 import astropy.units as u
 import lalsimulation.gwsignal.core.waveform as wfm
 
+# ----- Local Package Imports -----
 from .inner_product import inner_product, inner_product_computation, norm
-from .waveform_utils import restrict_f_range
+from .waveform_utils import get_strain
 from .matrix_with_units import MatrixWithUnits
-from .fisher_utils import fisher_matrix
+from .fisher_utils import (
+    fisher_matrix, get_waveform_derivative_1D,
+    get_waveform_derivative_1D_with_convergence
+)
 
 
-# class FisherMatrix:
-class FisherMatrix(MatrixWithUnits):
+class FisherMatrix:
+    """
+    A data type tailored to Fisher matrices. It stores the Fisher matrix
+    itself, along with its inverse.
+    its inverse
+
+    Parameters
+    ----------
+    wf_params_at_point : _type_
+        _description_
+    params_to_vary : _type_
+        _description_
+    wf_generator : _type_
+        _description_
+
+        A convenient option to use the method `FisherMatrix.
+        get_wf_generator`, which generates a suitable function from
+        a few arguments.
+
+    See also
+    --------
+    ~gwsignal.fisher_utils.fisher_matrix : Routine used for calculation
+    of the Fisher matrix.
+    numpy.linalg.inv : Routine used for inversion of the Fisher matrix.
+    """
+
     default_metadata = {
         'convergence_check': 'diff_norm',
         'break_upon_convergence': True,
-        'convergence_threshold': 0.01,
-        'return_info': True,
+        'convergence_threshold': 0.01
     }
 
-    # def __init__(self, convergence_check: str = None):
-    # def __init__(self, wf_params_at_point, metadata: dict[str, Any] = None):
-    def __init__(self, wf_params_at_point, params_to_vary, wf_generator, **metadata):  # Just use *args?
-        # Hmmm, just collect all kwargs in metadata? Then not check for None,
-        # can just do else case all the time
-        # if metadata is None:
-        #     self.metadata = self.default_metadata
-        # else:
-        #     self.metadata = self.default_metadata | metadata
+    def __init__(self,
+            wf_params_at_point: dict[str, u.Quantity],
+            params_to_vary: str,
+            wf_generator: Callable[[dict[str, u.Quantity]],
+                                   FrequencySeries | ArrayLike],
+            **metadata
+        ):
+        """
+        Initialize a ``FisherMatrix``.
+        """
 
+        self.wf_params_at_point = wf_params_at_point
+        self.params_to_vary = params_to_vary
+        self.wf_generator = wf_generator
+        self.metadata = self.default_metadata | metadata
 
-        self.metadata = self.default_metadata #| metadata
-        
-        # self.fisher = self.calc_fisher_matrix_at_point(
-        # self.fisher = fisher_matrix(
-        #     wf_params_at_point,
-        #     params_to_vary,
-        #     wf_generator,
-        #     **metadata
+        if ('return_info' in metadata.keys()) and metadata['return_info']:
+            logging.info(
+                'The `return_info` key is set to true to collect information '
+                'for further use in the class.'
+            )
+
+            self.metadata['return_info'] = True
+    
+
+        # Dummy Fisher, speed up when testing
+        # self._fisher = MatrixWithUnits(
+        #     np.full((2, 2), 42),
+        #     np.full((2, 2), u.s)
         # )
 
-        # Maybe better: set property wf_params_at_point and in setter for this,
-        # the fisher matrix is recomputed
-        self.wf_params_at_point = wf_params_at_point
-        
-        ...
-        """
-        Other idea: make function self.update_metadata and then recompute
-        Fisher after that? Because if we do that for wf_params_at_point etc
-        one by one, we will recompute Fisher several times, which is not wanted
+        self._fisher, self._deriv_info = fisher_matrix(
+            self.wf_params_at_point,
+            self.params_to_vary,
+            self.wf_generator,
+            **self.metadata
+        )
+        # NOTE: although it may not be good practice to set private attributes
+        # like self._fisher, this is our workaround to make self.fisher
+        # immutable (has no setter). If we were to set self.fisher here,
+        # a setter would be required
+        # -> _fisher being set is inevitable, some property has to be settable
 
-        TODOs:
-        - perform parameter_check manually?
-        - perform conversion of matrix to certain list standard units?
-        """
-
-
-    # @property
-    # def wf_params_at_point(self):
-    #     return self._wf_params_at_point
     
-    # @wf_params_at_point.setter
-    # def wf_params_at_point(self, wf_params_at_point):
-    #     self._wf_params_at_point = wf_params_at_point
-
-    #     # TODO: decide if self.fisher or self._fisher shall be set
-    #     self.fisher = fisher_matrix(
-    #         self._wf_params_at_point,
-    #         self.params_to_vary
-    #     )
-    
-
     @property
     def fisher(self):
         try:
@@ -84,6 +108,7 @@ class FisherMatrix(MatrixWithUnits):
                 return self._fisher
         except AttributeError:
             pass
+            # return None here?
 
         # Either it was None or not set. Either way, recompute
         # self._fisher = fisher_matrix(
@@ -91,39 +116,176 @@ class FisherMatrix(MatrixWithUnits):
         #     params_to_vary=params_to_vary,
         # )
 
+        # With new structure, just create new class?
+        # -> on the other hand, this should never happen...
+
 
         return self._fisher
     
-    @fisher.setter
-    def fisher(self, value):
-        # Definitely think about conditions/consequences that setting has
-        ...
-        self._fisher = value
+
+    # @fisher.setter
+    # def fisher(self, value):
+    #     try:
+    #         logging.info('Here we are')
+    #         attr_test = self._fisher
+
+    #         raise AttributeError(
+    #             'Attribute `fisher` may only be set upon initialization of an '
+    #             'instance of ``FisherMatrix``.'
+    #         )
+    #     except (AttributeError) as err:
+    #         err_msg = str(err)
+    #         if not ('upon initialization') in err_msg:
+    #             self._fisher = value
+    #         else:
+    #             raise AssertionError(err_msg)
+            
+    
+    # _fisher = property(self.fisher.__get__)
     
 
     @property
-    def inverse(self):
+    def fisher_inverse(self):
         # TODO: decide if it shall be computed upon call or upon calculation of Fisher
         try:
-            if self._inverse is not None:
-                return self._inverse
+            return self._fisher_inverse
         except AttributeError:
-            pass
+            self._fisher_inverse = MatrixWithUnits(
+                np.linalg.inv(self.fisher.value),
+                self.fisher.unit**-1,
+            )
 
-        # Either it was None or not set. Either way, recompute
-        self._inverse = MatrixWithUnits(
-            np.linalg.inv(self.fisher.value),
-            self.fisher.unit**-1,
-        )
+            return self._fisher_inverse
 
 
-        return self._inverse
+    def update_metadata(self,
+            new_wf_params_at_point: Optional[dict[str, u.Quantity]] = None,
+            new_params_to_vary: Optional[str | list[str]] = None,
+            new_wf_generator: Optional[Any] = None,
+            **new_metadata
+        ) -> Self:
+        """
+        Generate a Fisher matrix with properties like the current
+        instance has, but selected updates.
+        Note that this creates a new instance of ``FisherMatrix`` since
+        updating properties would require new calculation anyway.
+
+        Parameters
+        ----------
+        new_wf_params_at_point : _type_, optional
+            _description_, by default None
+        new_params_to_vary : _type_, optional
+            _description_, by default None
+        new_wf_generator : _type_, optional
+            _description_, by default None
+
+        Returns
+        -------
+        ~gw_signal_tools.fisher_matrix.FisherMatrix
+            New Fisher matrix, calculated with updated metadata.
+        """
+
+        if new_wf_params_at_point is None:
+            new_wf_params_at_point = self.wf_params_at_point
+
+        if new_params_to_vary is None:
+            new_params_to_vary = self.params_to_vary
+
+        if new_wf_generator is None:
+            new_wf_generator = self.wf_generator
+        
+        if len(new_metadata) > 0:
+            new_metadata = self.metadata | new_metadata
+
+
+        return FisherMatrix(new_wf_params_at_point, new_params_to_vary,
+                            **new_metadata)
 
     
-    # @property
     def condition_number(self,
-                         p: Optional[float | Literal['fro', 'nuc']] = 'fro'):
-        return np.linalg.cond(self.fisher.value, p)
+                         matrix_norm: float | Literal['fro', 'nuc'] = 'fro'):
+        """
+        Condition number of the Fisher matrix.
+
+        Parameters
+        ----------
+        matrix_norm : float | Literal['fro', 'nuc'], optional,
+        default = 'fro'
+            Matrix norm that shall be used for the calculation. Must be
+            compatible with argument `p` of `~numpy.linalg.cond`.
+
+        Returns
+        -------
+        float
+            Condition number of `self.fisher`.
+
+        See also
+        --------
+        numpy.linalg.cond : Routine used for calculation.
+        """
+        return np.linalg.cond(self.fisher.value, p=matrix_norm)
+
+
+    def project_fisher(self, projection_params):
+        # Idea: pply projection onto certain parameters
+        # -> can this be applied to multiple ones?
+        raise NotImplementedError
+    
+
+    def plot(self):
+        # plot with color=uncertainty
+        raise NotImplementedError
+    
+
+    @staticmethod
+    def get_wf_generator(approximant: str, domain: str, *args, **kwargs
+                         ) -> Callable[[dict[str, u.Quantity]],
+                                       FrequencySeries | ArrayLike]:
+        """
+        Generates a function that fulfils the requirements of the
+        `wf_generator` argument of a ``FisherMatrix``.
+
+        Parameters
+        ----------
+        approximant : _type_
+            _description_
+        domain : _type_
+            _description_
+
+        Returns
+        -------
+        _type_
+            _description_
+
+        See also
+        --------
+        gw_signal_tools.waveform_utils.get_strain :
+            Function that is wrapped here. All arguments provided in
+            addition to the mandatory ones are passed to this function
+            (just like `domain` is as well).
+        lalsimulation.gwsignal.core.waveform.
+        LALCompactBinaryCoalescenceGenerator : 
+            Function used to get a generator from `approximant`. This is
+            passed to the `generator` argument of `get_strain`.
+        """
+        print(approximant)
+        generator = wfm.LALCompactBinaryCoalescenceGenerator(approximant)
+
+        def wf_generator(wf_params):
+            return get_strain(wf_params, domain, generator, *args, **kwargs)
+
+        return wf_generator
+    
+
+    # ----- Set some Python class related goodies -----
+    def __repr__(self) -> str:
+        return self.fisher.__repr__()
+        # TODO: make custom one with more information
+        # return '''
+        # Fisher Matrix
+
+        # MORE DESCRIPTION TO COME
+        # '''
     
 
     def __array__(self):
@@ -131,54 +293,6 @@ class FisherMatrix(MatrixWithUnits):
         # return self.fisher.value
         return self.fisher.__array__()
 
-    """
-    Idea: we could store Fisher matrix and inverse in here and also
-    diagonal version, from which we would get condition number.
-    
-    -> maybe just compute Fisher and diagonal; then inform about
-    condition number when inverse is called
-
-    -> ah, condition number is norm(A)*norm(A^-1); so computing inverse
-    would also be just fine (depends on norm used)
-
-
-    Make property deriv_info, where starting frequency etc can be set?
-    Can also set all step_sizes and resetting would probably be good
-    idea (back to defaults)
-
-
-    Store in pandas frame? Could be useful for units
-
-    
-    Store derivatives?
-    """
-
-
-    def derivative_1D():
-        # Derivative with convergence
-
-        # Also allow input like convergence_check and if None, take the
-        # one defined in class -> gives possibility to play around with
-        # settings without changing class attributes
-        raise NotImplementedError
-    
-    def calc_fisher_matrix_at_point(
-        wf_params_at_point: dict[str, float | u.Quantity]
-    ) -> np.ndarray:
-        raise NotImplementedError
-
-    # def fisher_matrix_projected():
-    def project_fisher():
-        # Apply projection onto certain parameters
-        raise NotImplementedError
-    
-
-    def eval_at_point():
-        # Could then update Fisher etc based on that
-        # -> potentially convenient because one can use a class that
-        #    has been setup for multiple points
-        raise NotImplementedError
-    
 
     # def __float__():
     #     # Idea: return only matrix with values, not what is usually
@@ -186,14 +300,9 @@ class FisherMatrix(MatrixWithUnits):
     #     return 
     # Does not work, float() wants real float returned, not array with this dtype
 
-    @classmethod
-    def plot(self):
-        ...
-        # plot with color=uncertainty
 
-
-def fisher_inverse(matrix: np.ndarray) -> np.ndarray:
-    return np.linalg.inv(matrix)
-
-def condition_number(matrix: np.ndarray, inverse: np.ndarray) -> float:
-    return np.linalg.norm(matrix) * np.linalg.norm(inverse)
+    # def __getattr__(self, attr):
+    #     # return super().__getattribute__(attr)
+    #     return self._fisher.__getattribute__(attr)
+    # Thought might be a good idea to get stuff like array or repr
+    # from .fisher, which seemed like very intuitive way to do stuff
---
+++ tests/test_fisher_matrix.py
@@ -0,0 +1,88 @@
+# ----- Standard Lib Imports -----
+import unittest
+
+# ----- Third Party Imports -----
+import numpy as np
+
+import astropy.units as u
+
+# ----- Local Package Imports -----
+from gw_signal_tools.inner_product import norm
+from gw_signal_tools.matrix_with_units import MatrixWithUnits
+from gw_signal_tools.fisher_matrix import FisherMatrix
+from gw_signal_tools.fisher_utils import (
+    fisher_matrix, get_waveform_derivative_1D,
+    get_waveform_derivative_1D_with_convergence
+)
+
+
+#%% Initializing commonly used variables
+f_min = 20.*u.Hz
+f_max = 1024.*u.Hz
+
+wf_params = {
+    'total_mass': 100.*u.solMass,
+    'mass_ratio': 0.42*u.dimensionless_unscaled,
+    'deltaT': 1./2048.*u.s,
+    'f22_start': f_min,
+    'f_max': f_max,
+    'f22_ref': 20.*u.Hz,
+    'phi_ref': 0.*u.rad,
+    'distance': 1.*u.Mpc,
+    'inclination': 0.0*u.rad,
+    'eccentricity': 0.*u.dimensionless_unscaled,
+    'longAscNodes': 0.*u.rad,
+    'meanPerAno': 0.*u.rad,
+    'condition': 0
+}
+
+approximant = 'IMRPhenomXPHM'
+
+phenomx_generator = FisherMatrix.get_wf_generator(approximant, 'frequency')
+
+
+fisher_tot_mass = FisherMatrix(
+    wf_params,
+    'total_mass',
+    wf_generator=phenomx_generator,
+    return_info=True
+)
+
+
+#%% Simple consistency tests
+def test_unit():
+    assert fisher_tot_mass.fisher[0, 0].unit == 1/u.solMass**2
+
+def test_inverse():
+    assert np.all(np.equal(np.linalg.inv(fisher_tot_mass.fisher.value),
+                           fisher_tot_mass.fisher_inverse.value))
+    
+    assert np.all(np.equal(fisher_tot_mass.fisher.unit**-1,
+                           fisher_tot_mass.fisher_inverse.unit))
+    
+
+def test_fisher_calc():
+    fisher_tot_mass_2 = fisher_matrix(wf_params, 'total_mass',
+                                      phenomx_generator)
+
+    assert fisher_tot_mass.fisher == fisher_tot_mass_2
+
+
+def test_criterion_consistency():
+    fisher_tot_mass_2 = fisher_matrix(wf_params, 'total_mass',
+                                      phenomx_generator, convergence_check='mismatch')
+    
+    assert fisher_tot_mass.fisher == fisher_tot_mass_2
+    # Interesting, even equal. Roughly equal would also be sufficient, might
+    # converge at different step sizes and thus have slightly different results
+
+
+#%% Confirm that certain errors are raised
+class ErrorRaising(unittest.TestCase):
+    def test_immutable(self):
+        # Setting Fisher matrix related attributes should throw error
+        with self.assertRaises(AttributeError):
+            fisher_tot_mass.fisher = 42
+    
+        with self.assertRaises(AttributeError):
+            fisher_tot_mass.fisher_inverse = 42

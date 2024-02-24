First real testing of derivative and Fisher consistency. Fixed a bug related to min_dev_index in derivative function.

gw_signal_tools/fisher_utils.py
@@ -344,8 +344,11 @@ def get_waveform_derivative_1D_with_convergence(
     """
     
     if step_sizes is None:
+        # step_sizes = np.reshape(np.outer([1e-2, 1e-3, 1e-4, 1e-5, 1e-6], [5, 1]), -1)  # Back to relative one
+        # step_sizes = np.reshape(np.outer([1e-2, 1e-3, 1e-4], [5, 1]), -1)  # Back to relative one -> testing
         step_sizes = np.reshape(np.outer([1e-2, 1e-3, 1e-4, 1e-5], [5, 1]), -1)  # Seems most reasonable choice at the moment -> testing showed that 1e-6 might be too small for good results :OO
         # step_sizes = np.reshape(np.outer([1e-3, 1e-4, 1e-5], [5, 1]), -1)  # 1e-2 might be too large for some parameters -> on the other hand, 1e-5 too small for others. So there is no perfect choise
+        # step_sizes = np.reshape(np.outer([1e-3, 1e-4, 1e-5, 1e-6, 1e-7, 1e-8], [5, 1]), -1)  # For more detailed testing of convergence
         # step_sizes = np.reshape(np.outer([1e-3, 1e-4, 1e-5, 1e-6, 1e-7, 1e-8], [5, 1]), -1)  # For more detailed testing of convergence
 
 
@@ -382,6 +385,7 @@ def get_waveform_derivative_1D_with_convergence(
             #     step_size
             # )
 
+
             try:
                 deriv_param = get_waveform_derivative_1D(
                     wf_params_at_point,
@@ -403,7 +407,9 @@ def get_waveform_derivative_1D_with_convergence(
                     # indices become inconsistent with step_sizes
                     derivative_vals += [0.0]
                     deriv_norms += [np.inf]
-                    convergence_vals += [np.inf]
+                    convergence_vals += [np.inf]  # Hmm, but we append below with certain condition, right? -> but that was also wrong...
+                    # if len(derivative_vals) >= 2:
+                    #     convergence_vals += [np.inf]
 
                     continue
                 else:
@@ -430,8 +436,9 @@ def get_waveform_derivative_1D_with_convergence(
                             norm(deriv_param - derivative_vals[-2],
                                  **inner_prod_kwargs) \
                             / np.sqrt(derivative_norm)
-                        ]  # Index -1 is deriv_param
+                        ]
                     else:
+                        convergence_vals += [np.inf]
                         continue
                 case 'mismatch':
                     # Compute mismatch, using that we already know norms
@@ -442,11 +449,12 @@ def get_waveform_derivative_1D_with_convergence(
                             / np.sqrt(derivative_norm * deriv_norms[-2])
                         ]  # Index -1 is deriv_param
                     else:
+                        convergence_vals += [np.inf]
                         continue
 
 
-            logging.debug(convergence_vals)
-            logging.debug([len(derivative_vals), i, len(convergence_vals)])
+            logging.info(convergence_vals)
+            logging.info([len(derivative_vals), i, len(convergence_vals)])
 
 
             if (len(convergence_vals) >= 2
@@ -456,20 +464,27 @@ def get_waveform_derivative_1D_with_convergence(
 
                 if break_upon_convergence:
                     min_dev_index = i  # Then it can also be used to access step_sizes
+                    logging.info([min_dev_index, step_size])
                     break
         
 
-        # Check (i) if we shall break upon convergence and in case yes (ii) if
+        # Check (i) if we shall break upon convergence and in case no (ii) if
         # convergence was reached using these step sizes (refine them if not)
         if not break_upon_convergence or not is_converged:
-            min_dev_index = np.nanargmin(convergence_vals)  # Should not have nan, but who knows
+            # min_dev_index = np.nanargmin(convergence_vals)  # Should not have nan, but who knows
+            # TODO: is this consistent? Because convergence_vals has one less element than other stuff...
             # TODO: rename min_dev_index
+            min_dev_index = np.nanargmin(convergence_vals) #+ 1  # Should not have nan, but who knows
 
             # Cut steps made around step size with best criterion value in half
             # compared to current steps (we take average step size in case
             # difference to left and right is unequl)
             left_step = (step_sizes[min_dev_index - 1] - step_sizes[min_dev_index]) / 4.0
-            right_step = (step_sizes[min_dev_index + 1] - step_sizes[min_dev_index]) / 4.0
+            if min_dev_index < len(step_sizes) - 1:
+                right_step = (step_sizes[min_dev_index + 1] - step_sizes[min_dev_index]) / 4.0
+            else:
+                # Index + 1 is invalid
+                right_step = left_step
             # 4.0 due to factor of two in step_sizes below
             step_sizes = step_sizes[min_dev_index] + np.array(
                 [2.0 * left_step, 1.0 * left_step, 1.0 * right_step, 2.0 * right_step]
@@ -487,7 +502,7 @@ def get_waveform_derivative_1D_with_convergence(
             f'for parameter `{param_to_vary}` using convergence check method '
             f'`{convergence_check}`, even after {refine_numb} refinements of '
             'step sizes. The minimal value of the criterion was '
-            f'{convergence_vals[min_dev_index]}, which is above the selected '
+            f'{convergence_vals[min_dev_index - 1]}, which is above the selected '
             f'threshold of {convergence_threshold}.'
             'If you are not satisfied with the result (for an eye test, you '
             'can plot the `convergence_plot` value returned in case '
@@ -496,11 +511,13 @@ def get_waveform_derivative_1D_with_convergence(
     
 
     if return_info:
+        # TODO: rethink of plot made, seems to create axis each time and
+        # consume quite some memory
         fig, ax = plt.subplots()
 
         for i in range(len(derivative_vals)):
-            ax.plot(derivative_vals[i].real, '--', label=f'{step_sizes[i]} (Re)')
-            ax.plot(derivative_vals[i].imag, ':', label=f'{step_sizes[i]} (Im)')
+            ax.plot(derivative_vals[i].real, '--', label=f'{step_sizes[i]:.6f} (Re)')
+            ax.plot(derivative_vals[i].imag, ':', label=f'{step_sizes[i]:.6f} (Im)')
 
         ax.legend(title='Step Sizes', ncols=max(1, len(derivative_vals) % 3))
 
@@ -515,9 +532,10 @@ def get_waveform_derivative_1D_with_convergence(
         return derivative_vals[min_dev_index], {
             'norm_squared': deriv_norms[min_dev_index],
             'final_step_size': step_sizes[min_dev_index],
-            'final_convergence_val': convergence_vals[min_dev_index - 1 if min_dev_index != -1 else -1],
+            # 'final_convergence_val': convergence_vals[min_dev_index - 1 if min_dev_index != -1 else -1],  # -1 should not occur, right?
+            'final_convergence_val': convergence_vals[min_dev_index],  # -1 should not occur anymore
             'number_of_refinements': refine_numb,
-            'final_set_of_rel_step_sizes': step_sizes,
+            'final_set_of_step_sizes': step_sizes,
             'convergence_plot': ax
         }
     else:
@@ -570,6 +588,7 @@ def get_waveform_derivative_1D(
 
     param_center_val = wf_params_at_point[param_to_vary]
     step_size = u.Quantity(step_size, unit=param_center_val.unit)
+    # step_size = step_size * param_center_val
     param_vals = param_center_val + np.array([-2., -1., 1., 2.]) * step_size
 
 

tests/test_fisher_utils.py
@@ -3,18 +3,22 @@ import unittest
 
 # ----- Third Party Imports -----
 import numpy as np
+from numpy.testing import assert_allclose
+import matplotlib.pyplot as plt
 import astropy.units as u
 from gw_signal_tools.test_utils import (
-    assert_allclose_quantity, assert_allclose_frequseries,
-    assert_allclose_timeseries
+    assert_allclose_quantity, assert_allclose_MatrixWithUnits,
+    assert_allclose_frequseries
 )
 from gwpy.testing.utils import assert_quantity_equal
+
+import pytest
 
 # ----- Local Package Imports -----
 from gw_signal_tools.fisher_utils import (
     num_diff, get_waveform_derivative_1D,
     get_waveform_derivative_1D_with_convergence,
-    fisher_matrix, fisher_element
+    fisher_matrix
 )
 from gw_signal_tools.fisher_matrix import FisherMatrix
 
@@ -26,14 +30,14 @@ def test_num_diff():
 
     derivative_vals = num_diff(x_vals, h=step_size)
 
-    assert np.all(np.isclose(derivative_vals, np.ones(derivative_vals.size), atol=0.0, rtol=0.01))
+    assert_allclose(derivative_vals, np.ones(derivative_vals.size), atol=0.0, rtol=0.01)
 
     func_vals = 0.5 * x_vals**2
     derivative_vals = num_diff(func_vals, h=step_size)
 
-    assert np.all(np.isclose(derivative_vals[2 : -2], x_vals[2 : -2], atol=0.0, rtol=0.01))
-    assert np.all(np.isclose(derivative_vals[1:2], x_vals[1:2], atol=0.0, rtol=0.01))  # First correct value is zero, thus relative deviation is always 1
-    assert np.all(np.isclose(derivative_vals[-2:], x_vals[-2:], atol=0.0, rtol=0.01))
+    assert_allclose(derivative_vals[2 : -2], x_vals[2 : -2], atol=0.0, rtol=0.01)
+    assert_allclose(derivative_vals[1:2], x_vals[1:2], atol=0.0, rtol=0.01)  # First correct value is zero, thus relative deviation is always 1
+    assert_allclose(derivative_vals[-2:], x_vals[-2:], atol=0.0, rtol=0.01)
     # NOTE: for values at border of interval, rule is not applicable.
     # Thus we make separate checks, methods could be less accurate there
 
@@ -41,17 +45,12 @@ def test_num_diff():
 
     derivative_vals = num_diff(func_vals, h=step_size)
 
-    assert np.all(np.isclose(derivative_vals[2 : -2], np.cos(x_vals)[2 : -2], atol=0.0, rtol=0.01))
-    assert np.all(np.isclose(derivative_vals[:2], np.cos(x_vals)[:2], atol=0.0, rtol=0.01))
-    assert np.all(np.isclose(derivative_vals[-2:], np.cos(x_vals)[-2:], atol=0.0, rtol=0.02))
+    assert_allclose(derivative_vals[2 : -2], np.cos(x_vals)[2 : -2], atol=0.0, rtol=0.01)
+    assert_allclose(derivative_vals[:2], np.cos(x_vals)[:2], atol=0.0, rtol=0.01)
+    assert_allclose(derivative_vals[-2:], np.cos(x_vals)[-2:], atol=0.0, rtol=0.02)
 
 
-def test_wf_deriv():
-    # TODO: compare with numdifftools
-    ...
-
-
-#%% Initializing commonly used variables
+#%% ----- Initializing commonly used variables for Fisher tests -----
 f_min = 20.*u.Hz
 f_max = 1024.*u.Hz
 
@@ -71,31 +70,150 @@ wf_params = {
     'condition': 0
 }
 
+# test_params = ['total_mass', 'distance']
+test_params = ['total_mass', 'distance', 'mass_ratio']
+
+
 approximant = 'IMRPhenomXPHM'
 
-phenomx_generator = FisherMatrix.get_wf_generator(approximant, 'frequency')
+phenomx_generator = FisherMatrix.get_wf_generator(approximant)
 
-deriv = get_waveform_derivative_1D_with_convergence(
-    wf_params,
-    'total_mass',
-    phenomx_generator,
-    return_info=False
-)
-
-print(deriv)
-
-fisher_val, info = fisher_matrix(
-    wf_params,
-    # 'total_mass',
-    ['total_mass', 'distance'],
-    phenomx_generator,
-    return_info=True
-)
-
-print(fisher_val)
-print(fisher_val.value)
-print(fisher_val.unit)
-print(fisher_val[0, 0].to(1/(u.Msun)**2))
+# plt.plot(phenomx_generator(wf_params).real)
+# plt.plot(phenomx_generator(wf_params).imag)
+# plt.show()
 
 
-# Do some tests
+#%% ----- Derivative consistency checks -----
+def test_wf_deriv_numdifftools():
+    # TODO: compare with numdifftools
+    ...
+
+
+@pytest.mark.parametrize('param', test_params)
+@pytest.mark.parametrize('q_val', [0.42, 0.05])
+@pytest.mark.parametrize('break_conv', [True, False])
+def test_step_size(param, q_val, break_conv):
+    deriv, deriv_info = get_waveform_derivative_1D_with_convergence(
+        wf_params | {'mass_ratio': q_val*u.dimensionless_unscaled},
+        param,
+        phenomx_generator,
+        break_upon_convergence=break_conv,
+        return_info=True
+    )
+    plt.close()
+
+    deriv_fixed_step_size = get_waveform_derivative_1D(
+        wf_params | {'mass_ratio': q_val*u.dimensionless_unscaled},
+        param,
+        phenomx_generator,
+        step_size=deriv_info['final_step_size']
+    )
+
+    # These must be equal (not just close)
+    if break_conv:
+        assert_allclose_frequseries(deriv, deriv_fixed_step_size, atol=0.0, rtol=0.0)
+    else:
+        deriv.crop(end=256 * u.Hz, copy=False)
+        deriv_fixed_step_size.crop(end=256 * u.Hz, copy=False)
+
+        # assert_allclose_frequseries(deriv, deriv_fixed_step_size, atol=2e-24, rtol=6e-4)
+        if param != 'total_mass':
+            assert_allclose_frequseries(deriv, deriv_fixed_step_size, atol=2e-24, rtol=2e-3)
+        else:
+            # One peak for q=0.42 where deviation is larger than otherwise.
+            # No idea where this comes from
+            assert_allclose_frequseries(deriv, deriv_fixed_step_size, atol=2e-23, rtol=2e-3)
+        # Not sure why, but they are never fully equal here. Maybe we are off
+        # by one index (though I checked this), but results look very equal
+        # and all deviations are around zeros, where small deviations result
+        # in large errors. Fisher matrix values below are very similar even
+        # for break_upon_convergence=False, which is only thing that counts
+        # Moreover, q=0.05 produces VERY challenging waveforms, this is merely
+        # to test if index error is handled correctly
+
+
+    # For eye test, this is where maximum deviation occurs
+    # -> imaginary part seems to be source of error
+    # deriv, deriv_info = get_waveform_derivative_1D_with_convergence(
+    #     wf_params,
+    #     'total_mass',
+    #     phenomx_generator,
+    #     return_info=True,
+    #     break_upon_convergence=False
+    # )
+
+    # deriv2 = get_waveform_derivative_1D(
+    #     wf_params,# | {'mass_ratio': 0.05*u.dimensionless_unscaled},
+    #     'total_mass',
+    #     phenomx_generator,
+    #     step_size=deriv_info['final_step_size']
+    # )
+
+    # print(deriv_info['final_step_size'])
+    # plt.close()
+    # plt.plot(deriv.real)
+    # plt.plot(deriv.imag)
+    # plt.plot(deriv2.real, '--')
+    # plt.plot(deriv2.imag, '--')
+    # # plt.plot(np.abs(deriv))
+    # # plt.plot(np.abs(deriv), '--')
+    # plt.plot(np.abs(deriv - deriv2)*50, label='Difference')
+    # plt.xlim(130, 170)
+    # # plt.ylim(-5e-22, 5e-22)
+    # plt.legend()
+    # plt.show()
+
+
+#%% ----- Fisher consistency checks -----
+@pytest.mark.parametrize('break_conv', [True, False])
+def test_convergence_check(break_conv):
+    fisher_diff_norm = fisher_matrix(
+        wf_params,
+        test_params,
+        phenomx_generator,
+        convergence_check='diff_norm',
+        break_upon_convergence=break_conv
+    )
+    plt.close()
+    
+    fisher_mismatch = fisher_matrix(
+        wf_params,
+        test_params,
+        phenomx_generator,
+        convergence_check='mismatch',
+        break_upon_convergence=break_conv
+    )
+    plt.close()
+
+    if break_conv:
+        assert_allclose_MatrixWithUnits(fisher_diff_norm, fisher_mismatch,
+                                        atol=0.0, rtol=0.0)
+    # 0.0 is good, means they converge at same step size, although this is
+    # surely not always the case
+    else:
+        assert_allclose_MatrixWithUnits(fisher_diff_norm, fisher_mismatch,
+                                        atol=0.0, rtol=1e-10)
+
+
+@pytest.mark.parametrize('crit', ['diff_norm', 'mismatch'])
+def test_break_upon_convergence(crit):
+    fisher_without_convergence = fisher_matrix(
+        wf_params,
+        test_params,
+        phenomx_generator,
+        convergence_check=crit,
+        break_upon_convergence=True
+    )
+
+    fisher_with_convergence = fisher_matrix(
+        wf_params,
+        test_params,
+        phenomx_generator,
+        convergence_check=crit,
+        break_upon_convergence=False
+    )
+
+    plt.close()
+
+    assert_allclose_MatrixWithUnits(fisher_without_convergence, fisher_with_convergence, atol=0.0, rtol=1e-3)
+    # Small deviations are expected, different final step sizes might be selected
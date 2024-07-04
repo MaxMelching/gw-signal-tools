Extending network tests. Fixed some bugs in systematic error calculation of network. Also had to add opt_bias to return in optimization_info.

gw_signal_tools/fisher/fisher_network.py
@@ -74,12 +74,6 @@ class FisherMatrixNetwork(FisherMatrix):
     @property
     def detectors(self):
         return self._detectors
-
-    # @property    
-    # def _fisher_for_dets(self):
-    #     """List of Fisher matrices for detectors."""
-    #     ...
-    # TODO: check if needed. This job is taken by detector_fisher, right?
     
     def _index_from_det(self, det: Detector | str):
         """Get index for detector name."""
@@ -118,7 +112,7 @@ class FisherMatrixNetwork(FisherMatrix):
         params: str | list[str] | None = None,
         optimize: bool | str | list[str] = True,
         optimize_fisher: str | list[str] | None = None,
-        return_opt_info: bool = True,
+        return_opt_info: bool = False,
         **inner_prod_kwargs
     ) -> MatrixWithUnits | tuple[MatrixWithUnits, dict[str, Any]]:
         if isinstance(optimize, str):
@@ -133,16 +127,26 @@ class FisherMatrixNetwork(FisherMatrix):
         #    adjusted version of code from FisherMatrix.sys_error here
         #    (main cost is waveform generation and thus optimization)
 
-        sys_error_list = []
-        # vector_list = []
-        vector = MatrixWithUnits.from_numpy_array(np.zeros((len(self.params_to_vary), 1)))
-        fisher = MatrixWithUnits.from_numpy_array(np.zeros(2*(len(self.params_to_vary),)))
-        opt_bias = MatrixWithUnits.from_numpy_array(np.zeros((len(self.params_to_vary), 1)))
+        # sys_error_list = []
+        # # vector_list = []
+        # fisher = MatrixWithUnits(np.zeros(2*(len(self.params_to_vary),)), self.fisher.unit)
+        # opt_bias = MatrixWithUnits(np.zeros(len(self.params_to_vary)),
+        #     [self.wf_params_at_point[param].unit for param in self.params_to_vary]).reshape((len(self.params_to_vary), 1))
+        # vector = MatrixWithUnits(np.zeros((len(self.params_to_vary), 1)),
+        #     (fisher @ opt_bias).unit)
+        sys_error = 0.
+        fisher = 0.
+        opt_bias = 0.
+        vector = 0.
+        # 0. is most convenient way to initialize here, adding a
+        # MatrixWithUnits on top is allowed
+
         optimization_info = {}
 
         for i, det in enumerate(self.detectors):
-            sys_error_list += [
-                self.detector_fisher[det].systematic_error(
+            # sys_error_list += [
+                # self.detector_fisher(det).systematic_error(
+            sys_error = self.detector_fisher(i).systematic_error(
                     reference_wf_generator=reference_wf_generator,
                     params=None,  # Get all for now, filter before return
                     optimize=optimize,
@@ -150,32 +154,36 @@ class FisherMatrixNetwork(FisherMatrix):
                     return_opt_info=True,
                     **inner_prod_kwargs
                 )
-            ]
+            # ]
             # NOTE: every element is now tuple, so pay attention to indices
 
-            optimization_info[det] = sys_error_list[-1][1]
+            # optimization_info[det] = sys_error_list[-1][1]
+            optimization_info[det] = sys_error[1]
 
             if isinstance(optimize, bool) and not optimize:
-                # used_fisher = self.detector_fisher[det].fisher
-                used_fisher = self.detector_fisher[i].fisher  # Should be faster
-            # elif isinstance(optimize, bool) and not optimize:
+                # used_fisher = self.detector_fisher(det)
+                # used_fisher = self.detector_fisher(i)  # Should be faster
+                # used_opt_bias = MatrixWithUnits(np.zeros(sys_error_list[-1][0].shape), sys_error_list[-1][0].unit)
+                used_opt_bias = 0.
+            
+                if optimize_fisher is not None:
+                    # used_fisher = used_fisher.project_fisher(optimize_fisher).fisher
+                    used_fisher = sys_error[1]['opt_fisher'].fisher
+                else:
+                    # used_fisher = used_fisher.fisher
+                    used_fisher = self.detector_fisher(i).fisher
             else:
                 # Some kind of optimization was carried out, thus we
-                # can access
-                used_fisher = sys_error_list[-1][1].opt_fisher.fisher
+                # can access attribute in info dictionary
+                # used_fisher = sys_error_list[-1][1]['opt_fisher']
+                # used_opt_bias = sys_error_list[-1][1]['opt_bias']
+                used_fisher = sys_error[1]['opt_fisher'].fisher
+                used_opt_bias = sys_error[1]['opt_bias']
             
-            # TODO: do same check for projection
-            
-            # vector_list += [
-            #     used_fisher @ sys_error_list[-1][0]
-            # ]
-            vector += used_fisher @ sys_error_list[-1][0]
-            # Ah shit, the idea is good, but we have added opt_bias,
-            # which was not obtained via calculation with inverse matrix.
-            # -> maybe give opt_bias as output into info, so that we can
-            #    subtract it here?
-
-            opt_bias += 0.
+            # vector += used_fisher @ (sys_error_list[-1][0] - used_opt_bias)
+            vector += used_fisher @ (sys_error[0] - used_opt_bias)
+            opt_bias += used_opt_bias
+            fisher += used_fisher
 
         fisher_bias = MatrixWithUnits.inv(fisher) @ vector + opt_bias
         
@@ -195,7 +203,7 @@ class FisherMatrixNetwork(FisherMatrix):
         
             fisher_bias = fisher_bias[param_indices]
         
-        if optimize is False or return_opt_info is False:
+        if return_opt_info is False:
             return fisher_bias
         else:
             return fisher_bias, optimization_info
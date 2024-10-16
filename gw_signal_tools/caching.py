707227c8af7923d2f4a6e7d760168316c565ff29
Max Melching <max.melching@aei.mpg.de>
2024-10-16 14:00:13 +0200

    Adding context managers for caching.

--- gw_signal_tools/caching.py
+++ gw_signal_tools/caching.py
@@ -1,12 +1,15 @@
-# -- Third Party Imports
-from functools import cache#, lru_cache
+# -- Standard Lib Imports
+from functools import cache
 from typing import Callable
+import contextlib
+import logging
 
 # -- Third Party Imports
 from gw_signal_tools import logger
 
 
-__all__ = ('disable_caching', 'enable_caching', 'cache_func', 'use_caching')
+__all__ = ('disable_caching', 'enable_caching', 'cache_func', 'use_caching',
+           'enable_caching_locally', 'disable_caching_locally')
 
 
 def _dummy_cache(func):
@@ -17,9 +20,15 @@ def _dummy_cache(func):
 # -- Default setting: caching disabled
 cache_func: Callable = _dummy_cache
 use_caching: bool = False
+_calling_from_context: bool = False
+# TODO: think about whether this makes sense or not. If we take logging
+# level approach, then we also omit all other info messages emitted by
+# logger, not sure if I like that...
 
 def disable_caching():
-    logger.info('Disabling caching')
+    global _calling_from_context
+    if not _calling_from_context:
+        logger.info('Disabling caching')
 
     global cache_func
     cache_func = _dummy_cache
@@ -27,14 +36,60 @@ def disable_caching():
     global use_caching
     use_caching = False
 
+
 def enable_caching():
-    logger.info('Enabling caching')
+    global _calling_from_context
+    if not _calling_from_context:
+        logger.info('Enabling caching')
 
     global cache_func
     cache_func = cache
+    # TODO: should we use lru_cache with typed=True? Right now, I do not
+    # see why, but I might be missing something
 
     global use_caching
     use_caching = True
+
+
+@contextlib.contextmanager
+def enable_caching_locally():
+    # -- Save state of things that we change in context
+    _switch_off_caching = not use_caching
+    # original_level = logger.getEffectiveLevel()
+    # logger.setLevel(logging.WARNING)
+    global _calling_from_context
+    _calling_from_context = True
+
+    try:
+        enable_caching()
+        yield
+    finally:
+        # -- Restore state from before this function was called
+        if _switch_off_caching:
+            disable_caching()
+
+        # logger.setLevel(original_level)
+        _calling_from_context = False
+
+@contextlib.contextmanager
+def disable_caching_locally():
+    # -- Save state of things that we change in context
+    _turn_on_caching = use_caching
+    # original_level = logger.getEffectiveLevel()
+    # logger.setLevel(logging.WARNING)
+    global _calling_from_context
+    _calling_from_context = True
+
+    try:
+        disable_caching()
+        yield
+    finally:
+        # -- Restore state from before this function was called
+        if _turn_on_caching:
+            enable_caching()
+
+        # logger.setLevel(original_level)
+        _calling_from_context = False
 
 # TODO: maybe make custom cacher that tries to, but fails if input is
 # usual dictionary? Then we could control whether or not something is
--- tests/test_caching.py
+++ tests/test_caching.py
@@ -21,3 +21,39 @@ def test_enable_disable_calls():
 
     assert use_caching == False
     assert cache_func == _dummy_cache
+
+
+def test_context_enabling():
+    from gw_signal_tools.caching import (
+        disable_caching, enable_caching_locally
+    )
+
+    disable_caching()
+
+    from gw_signal_tools.caching import use_caching
+    assert use_caching == False
+
+    with enable_caching_locally():
+        from gw_signal_tools.caching import use_caching
+        assert use_caching == True
+    
+    from gw_signal_tools.caching import use_caching
+    assert use_caching == False
+
+
+def test_context_disabling():
+    from gw_signal_tools.caching import (
+        enable_caching, disable_caching_locally
+    )
+
+    enable_caching()
+
+    from gw_signal_tools.caching import use_caching
+    assert use_caching == True
+
+    with disable_caching_locally():
+        from gw_signal_tools.caching import use_caching
+        assert use_caching == False
+    
+    from gw_signal_tools.caching import use_caching
+    assert use_caching == True

0e051b55ccc94a38ddf20677a5db46348189688a
Max Melching <max.melching@aei.mpg.de>
2024-10-16 14:01:11 +0200

    Adding copy to HashableDict.

--- gw_signal_tools/types/hash_dict.py
+++ gw_signal_tools/types/hash_dict.py
@@ -9,15 +9,20 @@ __all__ = ('HashableDict', )
 class HashableDict(dict):
     """A class based on ``dict`` that can be hashed."""
     def __hash__(self):
-        # return hash((frozenset(self.keys()), frozenset(self.values())))
         return hash(frozenset(self.items()))
-        # Should work similarly to above one, no? But perhaps a little
-        # faster, only one iterator call
+        # return hash(frozenset(self.copy().items()))
+        # -- Could think about doing this, now that copy is available
     
-    # For convenience we overwrite merge with dictionaries so that
-    # a HashableDict is returned too
+    # -- For convenience we overwrite merge with dictionaries so that a
+    # -- HashableDict is returned too
     def __or__(self, value: Any) -> UnionType:
         return HashableDict(super().__or__(value))
     
     def __ror__(self, value: Any) -> UnionType:
         return HashableDict(super().__ror__(value))
+    
+    def copy(self):
+        return HashableDict(dict.copy(self))
+    
+    def __copy__(self):
+        return self.copy()
--- tests/test_hashdict.py
+++ tests/test_hashdict.py
@@ -6,9 +6,10 @@ def test_hashing():
     hash_dict = HashableDict(a=2)
 
     hash_dict.__hash__()
+    hash(hash_dict)
 
 
-def test_hashdict_union():
+def test_union():
     dict_no_hash = dict(a=1)
     dict_hash = HashableDict(a=2)
 
@@ -25,8 +26,20 @@ def test_hashdict_union():
     )  # ror method
 
     assert not isinstance(dict_no_hash, HashableDict)
-    # Ensure that dict is not recognized as HashableDict, would mean
-    # previous results are meaningless
+    # -- Ensure that dict is not recognized as HashableDict, would mean
+    # -- previous results are meaningless
 
     assert not isinstance(dict_no_hash | dict_no_hash, HashableDict)
-    # Ensure that interplay of regular dictionaries is unaffected
+    # -- Ensure that interplay of regular dictionaries is unaffected
+
+def test_copy():
+    # -- Make sure that copy is still a HashableDict
+    dict_hash = HashableDict(a=2)
+    dict_hash_copy = dict_hash.copy()
+    dict_hash_copy['a'] = 1
+
+    hash(dict_hash_copy)
+
+    assert dict_hash['a'] == 2  # Also check that copying itself works
+
+    hash(dict_hash.__copy__())

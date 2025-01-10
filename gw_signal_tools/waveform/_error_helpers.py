e2bf6370136abd96e1f853417acd54c9f6153113
Max Melching <max.melching@aei.mpg.de>
2025-01-10 11:50:45 +0100

    Applying black formatting to _error_helpers, hoping to resolve merge conflict.

--- gw_signal_tools/waveform/_error_helpers.py
+++ gw_signal_tools/waveform/_error_helpers.py
@@ -23,8 +23,13 @@ Need consistent (i.e. convertible) units for `%s` (%s) and `%s` (%s).
 '''
 
 
-def _q_convert(quant: float | u.Quantity, target_unit: u.Unit, arg1name: str,
-               arg2name: str, err_msg: Optional[str] = None) -> u.Quantity:
+def _q_convert(
+    quant: float | u.Quantity,
+    target_unit: u.Unit,
+    arg1name: str,
+    arg2name: str,
+    err_msg: Optional[str] = None,
+) -> u.Quantity:
     """Wrapper function for quantity conversion with custom error raising."""
     try:
         return u.Quantity(quant, unit=target_unit)
@@ -35,7 +40,9 @@ def _q_convert(quant: float | u.Quantity, target_unit: u.Unit, arg1name: str,
         if err_msg is not None:
             raise ValueError(err_msg)
         else:
-            raise ValueError(_UNIT_CONV_ERR % (arg1name, quant.unit, arg2name, target_unit))
+            raise ValueError(
+                _UNIT_CONV_ERR % (arg1name, quant.unit, arg2name, target_unit)
+            )
 
 
 def _compare_series(*s: list[Series]) -> None:
@@ -50,24 +57,28 @@ def _compare_series(*s: list[Series]) -> None:
         # -- Idea: performing every mutual comparison is not required
         _compare_series(s[-1], s[-2])
         _compare_series(s[:-1])
-        return None    
-    
+        return None
+
     # -- Leaves case with len(s)s == 2
     s1, s2 = s
-    if not allclose_quantity(s1.dx, s2.dx, atol=0., rtol=1e-5):
+    if not allclose_quantity(s1.dx, s2.dx, atol=0.0, rtol=1e-5):
         # -- Some arbitrary deviation allowed, in case of numerical differences
         # raise ValueError('Signals must have equal spacing on x-axis.')
         # raise ValueError('Signals do not have equal spacing on x-axis.')
-        raise ValueError(f'Signals do not have equal spacing on x-axis ({s1.dx} vs. {s2.dx}).')
+        raise ValueError(
+            f'Signals do not have equal spacing on x-axis ({s1.dx} vs. {s2.dx}).'
+        )
 
     # TODO: decide whether to compare whole frequency array or just
     # start and end frequency. But then things in between could go wrong
 
-    if not allclose_quantity(s1.xindex, s2.xindex, atol=0.5*s1.dx.value, rtol=0.):
+    if not allclose_quantity(s1.xindex, s2.xindex, atol=0.5 * s1.dx.value, rtol=0.0):
         # -- Note: this atol checks for equality up to sampling accuracy
         # -- Note: this automatically checks for equal size
-        raise ValueError('Signals must have sufficiently equal xindex. '
-                         'Maximum allowed deviation is 0.5*dx.')
+        raise ValueError(
+            'Signals must have sufficiently equal xindex. '
+            'Maximum allowed deviation is 0.5*dx.'
+        )
 
 
 def _assert_ft_compatible(*fs: list[FrequencySeries]) -> None:
@@ -79,17 +90,17 @@ def _assert_ft_compatible(*fs: list[FrequencySeries]) -> None:
         for val in fs:
             _assert_ft_compatible(val)
         return None
-    
+
     fs = fs[0]
-    assert (
-        allclose_quantity(fs.f0.value, 0.0, atol=0.0, rtol=0.0)
-        or allclose_quantity(-(fs.f0 + fs.df), fs.frequencies[-1],
-                             atol=fs.df.value, rtol=0.0)
-    ), ('All signals must start either at f=0 or be symmetric around f=0, '
+    assert allclose_quantity(fs.f0.value, 0.0, atol=0.0, rtol=0.0) or allclose_quantity(
+        -(fs.f0 + fs.df), fs.frequencies[-1], atol=fs.df.value, rtol=0.0
+    ), (
+        'All signals must start either at f=0 or be symmetric around f=0, '
         'where the latter refers to the case of an odd sample size. For an '
         'even sample size, on the other hand, the number of samples for '
         'positive frequencies is expected to be one less than the number of '
         'samples for negative frequencies, in accordance with the format '
         'expected by `~numpy.fft.ifftshift`. Note that the conditions just'
         'mentioned do not apply to the case of a starting frequency f=0, '
-        'where both even and odd sample sizes are accepted.')
+        'where both even and odd sample sizes are accepted.'
+    )

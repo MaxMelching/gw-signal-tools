4a366b3751be77b07da99de9111191329af8ee26
Max Melching <max.melching@aei.mpg.de>
2025-02-15 17:06:45 +0100

    Renaming stub, preparing for serious usage.

---
+++ gw_signal_tools/types/gwsignal_types.py
@@ -0,0 +1,21 @@
+from typing import Callable, Union, TypeAlias
+import astropy.units as u
+from gwpy.frequencyseries import FrequencySeries
+from gwpy.timeseries import TimeSeries
+
+
+__doc__ = """File for custom type definitions."""
+
+__all__ = ('FDWFGen', 'TDWFGen', 'WFGen', )
+
+
+# GWParams: TypeAlias = dict[str, u.Quantity]
+# FDWFGen: TypeAlias = Callable[[GWParams], FrequencySeries]
+# TDWFGen: TypeAlias = Callable[[GWParams], TimeSeries]
+# WFGen: TypeAlias = Union[FDWFGen, TDWFGen]
+
+# TODO: should we define GWParams?
+
+FDWFGen: TypeAlias = Callable[[dict[str, u.Quantity]], FrequencySeries]
+TDWFGen: TypeAlias = Callable[[dict[str, u.Quantity]], TimeSeries]
+WFGen: TypeAlias = Union[FDWFGen, TDWFGen]

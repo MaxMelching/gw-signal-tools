840657077d657f7a0d726a9199e9b94f4256e68f
Max Melching <max.melching@aei.mpg.de>
2025-11-20 01:37:16 +0000

    First progress on more flexible derivative class. Also adding a base class for things to inherit from.

--- gw_signal_tools/waveform/deriv.py
+++ gw_signal_tools/waveform/deriv.py
@@ -25,6 +25,13 @@ Module for the ``WaveformDerivative`` and
 __all__ = ('WaveformDerivative', 'WaveformDerivativeGWSignaltools')
 
 
+deriv_routine_class_map = {
+    'gw_signal_tools': 'WaveformDerivativeGWSignaltools',
+    'numdifftools': 'WaveformDerivativeNumdifftools',
+    'amplitude_phase': 'WaveformDerivativeAmplitudePhase',
+}
+
+
 class WaveformDerivative:
     r"""
     Constructor class for numerical derivative of waveforms. This class
@@ -34,8 +41,10 @@ class WaveformDerivative:
 
     Parameters
     ----------
-    deriv_routine : Literal['gw_signal_tools', 'numdifftools', 'amplitude_phase']
-        Available routines. Default is `'numdifftools'`.
+    deriv_routine : Literal
+        Available routines, i.e. they keys of
+        `~gw_signal_tools.waveform.deriv_routine_class_map`. Default
+        is `'numdifftools'`.
 
     Returns
     -------
@@ -66,20 +75,152 @@ class WaveformDerivative:
     """
 
     def __new__(cls, *args, **kw_args):
-        deriv_routine = kw_args.pop('deriv_routine', 'numdifftools')
-
-        match deriv_routine:
-            case 'gw_signal_tools':
-                return WaveformDerivativeGWSignaltools(*args, **kw_args)
-            case 'numdifftools':
-                return WaveformDerivativeNumdifftools(*args, **kw_args)
-            case 'amplitude_phase':
-                return WaveformDerivativeAmplitudePhase(*args, **kw_args)
-            case _:
-                raise ValueError(f"Invalid deriv_routine '{deriv_routine}'.")
+        try:
+            deriv_routine = kw_args.pop('deriv_routine', 'numdifftools')
+            deriv = globals()[deriv_routine_class_map[deriv_routine]]
+            return deriv(*args, **kw_args)
+        except KeyError:
+            raise ValueError(
+                f"Invalid deriv_routine '{deriv_routine}', it is not registered "
+                "in `~gw_signal_tools.waveform.deriv_routine_class_map`."
+            )
 
 
-class WaveformDerivativeGWSignaltools:
+class WaveformDerivativeBase:
+    def __init__(
+        self,
+        point: dict[str, u.Quantity],
+        param_to_vary: str,
+        wf_generator: WFGen,
+        *args,
+        **kwds,
+    ) -> None:
+        pass
+
+    def __call__(self, x=None) -> Any:
+        pass
+
+    _param_bound_storage = param_bounds.copy()
+
+    @property
+    def param_bounds(self, param: str) -> tuple[float, float]:
+        return self._param_bound_storage[param]
+
+    @param_bounds.setter
+    def param_bounds(self, param: str, bounds: list[float, float]) -> None:
+        """
+        Specify bounds for a parameter that does not have registered
+        bounds yet, or update parameter bounds.
+
+        Parameters
+        ----------
+        param : str
+            The parameter for which bounds shall be specified.
+        bounds : list[float, float]
+            Lower and upper bound.
+        """
+        assert len(bounds) == 2, 'Need exactly one lower and one upper bound.'
+        self._param_bound_storage[param] = bounds
+
+    # TODO: do we need test_point? I guess yeah; for autodiff, it can just check if the point itself is valid I guess?
+    def test_point(self) -> None:
+        """
+        Check if `self.point` contains potentially tricky values, e.g.
+        mass ratios close to 1. If yes, a subsequent adjustment takes
+        place.
+        """
+        default_bounds = (-np.inf, np.inf)
+        lower_bound, upper_bound = self._param_bound_storage.get(
+            self.param_to_vary, default_bounds
+        )
+        if self.param_to_vary == 'mass_ratio':
+            # -- Depending on chosen convention, bounds might have to be corrected
+            if self.param_center_val > 1:
+                lower_bound, upper_bound = self._param_bound_storage.get(
+                    'inverse_mass_ratio', default_bounds
+                )
+
+        _base_step = ...  # TODO: this is where you have to replace with the step size you use
+        _par_val = self.param_center_val.value
+        lower_violation = _par_val - _base_step <= lower_bound
+        upper_violation = _par_val + _base_step >= upper_bound
+
+        # -- Check if base_step needs change
+        if lower_violation and upper_violation:
+            self.step.base_step = min(_base_step / 2.0, self._default_base_step)
+        elif lower_violation and not upper_violation:
+            # -- Can only happen if method is not forward yet
+            self.method = 'forward'
+            self.step.base_step = min(_base_step / 2.0, self._default_base_step)
+        elif not lower_violation and upper_violation:
+            # -- Can only happen if method is not backward yet
+            self.method = 'backward'
+            self.step.base_step = min(_base_step / 2.0, self._default_base_step)
+
+    # -- In case calling seems unintiutive, create attribute
+    @property
+    def deriv(self) -> Any:
+        """Alias for calling with no arguments."""
+        return self.__call__()
+
+    # -- Define certain properties. These have NO setters, on purpose!
+    @property
+    def param_to_vary(self) -> str:
+        """
+        Parameter that derivative is taken with respect to.
+
+        :type: `str`
+        """
+        return self._param_to_vary
+
+    @property
+    def param_center_val(self) -> u.Quantity:
+        """
+        Value of `self.param_to_vary` at which derivative is taken by
+        default.
+
+        :type: `~astropy.units.Quantity`
+        """
+        return self._param_center_val
+
+    @property
+    def wf_generator(self) -> WFGen:
+        """
+        Generator for waveform model that is differentiated.
+
+        :type: `~gw_signal_tools.types.WFGen`
+        """
+        return self._wf_generator
+
+    @property
+    def point(self) -> dict[str, u.Quantity]:
+        """
+        Point in parameter space at which waveform is differentiated,
+        encoded as key-value pairs representing parameter-value pairs.
+
+        :type: `dict[str, ~astropy.units.Quantity]`
+        """
+        return self._point
+
+    @property
+    def deriv_info(self) -> dict[str, Any]:
+        """
+        Information about the calculated derivative, given as a
+        dictionary. All keys from this dictionary are also accessible
+        as a class attribute.
+        """
+        return self._deriv_info
+
+    @deriv_info.setter
+    def deriv_info(self, info: dict[str, Any]):
+        for key, val in info.items():
+            # -- Make each key from deriv_info available as attribute
+            setattr(self, key, val)
+
+        self._deriv_info = info
+
+
+class WaveformDerivativeGWSignaltools(WaveformDerivativeBase):
     r"""
     Calculate the derivative of an arbitrary waveform with respect to
     an arbitrary input parameter in frequency domain, using a selection
@@ -702,7 +843,7 @@ class WaveformDerivativeGWSignaltools:
             # -- Depending on chosen convention, bounds might have to be corrected
             if self.param_center_val > 1:
                 lower_bound, upper_bound = self._param_bound_storage.get(
-                    self.param_to_vary, default_bounds
+                    'inverse_mass_ratio', default_bounds
                 )
 
         lower_violation = self._lower_point_checker(step_size, lower_bound)

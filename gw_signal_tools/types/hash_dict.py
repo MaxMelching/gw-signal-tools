4cb9a72c42b508ea5cc7997158adfe93dd3241c5
Max Melching <mmelchin@caltech.edu>
2025-12-07 22:11:58 -0800

    Correcting type hint in HashableDict.

--- gw_signal_tools/types/hash_dict.py
+++ gw_signal_tools/types/hash_dict.py
@@ -1,5 +1,5 @@
 # -- Standard Lib Imports
-from types import UnionType
+from __future__ import annotations
 from typing import Any
 
 
@@ -7,19 +7,24 @@ __all__ = ('HashableDict',)
 
 
 class HashableDict(dict):
-    """A class based on ``dict`` that can be hashed."""
+    """
+    A class based on ``dict`` that can be hashed.
+
+    Note that hashing is based on the items of the dictionary, so the
+    hash value associated with an instance is unsafe in the sense that
+    it will change if the contents of the dictionary change. However,
+    this is fine for use within the scope of `gw_signal_tools`.
+    """
 
     def __hash__(self):
         return hash(frozenset(self.items()))
-        # return hash(frozenset(self.copy().items()))
-        # -- Could think about doing this, now that copy is available
 
     # -- For convenience we overwrite merge with dictionaries so that a
     # -- HashableDict is returned too
-    def __or__(self, value: Any) -> UnionType:
+    def __or__(self, value: Any) -> HashableDict:
         return HashableDict(super().__or__(value))
 
-    def __ror__(self, value: Any) -> UnionType:
+    def __ror__(self, value: Any) -> HashableDict:
         return HashableDict(super().__ror__(value))
 
     def copy(self):
